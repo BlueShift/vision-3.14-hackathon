6dd1b0f2ebf19139a02b365bed10dd662b6e1198
Dobre Stavrov <dobre@blueshift.mk>
2022-12-23 11:56:07 +0100

    update photobooth.py

--- tornado-warriors/photobooth.py
+++ tornado-warriors/photobooth.py
@@ -9,27 +9,35 @@ import glob
 import signal
 from PIL import Image
 
-pics = glob.glob('/run/shm/test*.jpg')
+check = 1
+activate_logic = True
+
+pics = glob.glob("/run/shm/test*.jpg")
 for t in range(0, len(pics)):
-  os.remove(pics[t])
+    os.remove(pics[t])
+
 
 def Camera_start(wx, hx):
-  global p
-  rpistr = \
-      'libcamera-vid -t 0 --framerate 30 --autofocus --codec mjpeg --segment 1 -n -o /run/shm/test%06d.jpg --width ' \
-      + str(wx) + ' --height ' + str(hx)
-  p = subprocess.Popen(rpistr, shell=True, preexec_fn=os.setsid)
+    global p
+    rpistr = (
+        "libcamera-vid -t 0 --framerate 30 --autofocus --codec mjpeg --segment 1 -n -o /run/shm/test%06d.jpg --width "
+        + str(wx)
+        + " --height "
+        + str(hx)
+    )
+    p = subprocess.Popen(rpistr, shell=True, preexec_fn=os.setsid)
+
 
 width = 640
 height = 480
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
 open_eyes_detected = 0
 smile_detected = 0
 face_detected = 0
 start = 0
-tilted = False
+tilted = True
 
 _img = None
 _x = 0
@@ -37,124 +45,234 @@ _y = 0
 _w = 0
 _h = 0
 
-cv2.namedWindow('Frame')
-Text = ''
+
+cv2.namedWindow("Frame")
+Text = ""
 ttrat = time.time()
 
-def mouse_action(event,x,y,flags,param):
-  global p
-  if event == cv2.EVENT_LBUTTONDOWN:
-    if len(eyes) == 2 and smile_detected == 0 and tilted is False:
-      os.killpg(p.pid, signal.SIGTERM)
-      cv2.destroyAllWindows()
-      cv2.imwrite('PHOTO_BOOTH.jpg', _img)
-      with Image.open('PHOTO_BOOTH.jpg') as img:
-        img.load()
-        cropped_img = img.crop((_x-70, _y-100, _x + _h + 70, _y + _h + 40 ))
-        cropped_img.show()
-    else:
-      print('PHOTO DEMANDS ARE NOT MET!')
 
-cv2.setMouseCallback('Frame', mouse_action)
+def mouse_action(event, x, y, flags, param):
+    global p
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(len(eyes))
+        print(smile_detected)
+        print(tilted)
+        if len(eyes) == 2 and smile_detected == 0 and tilted is False:
+            os.killpg(p.pid, signal.SIGTERM)
+            cv2.destroyAllWindows()
+            cv2.imwrite("PHOTO_BOOTH.jpg", _img)
+            with Image.open("PHOTO_BOOTH.jpg") as img:
+                img.load()
+                cropped_img = img.crop((_x - 70, _y - 100, _x + _h + 70, _y + _h + 40))
+                cropped_img.show()
+        else:
+            print("PHOTO DEMANDS ARE NOT MET!")
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        activate_logic = True
+        check = 1
+
+
+cv2.setMouseCallback("Frame", mouse_action)
+
+# start capturing images
 
 Camera_start(width, height)
 
+# main loop
+
+
 while True:
-  if time.time() - ttrat > 3 and ttrat > 0:
-    Text = ''
-    ttrat = 0
+    # remove message after 3 seconds
 
-  pics = glob.glob('/run/shm/test*.jpg')
-  while len(pics) < 2:
-    pics = glob.glob('/run/shm/test*.jpg')
+    if time.time() - ttrat > 3 and ttrat > 0:
+        Text = ""
+        ttrat = 0
 
-  pics.sort(reverse=True)
-  img = cv2.imread(pics[1])
-  _img = img
-  if len(pics) > 2:
-    for tt in range(2, len(pics)):
-      os.remove(pics[tt])
+    # load image
 
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-  face_detected = 0
+    pics = glob.glob("/run/shm/test*.jpg")
+    while len(pics) < 2:
+        pics = glob.glob("/run/shm/test*.jpg")
+    pics.sort(reverse=True)
+    img = cv2.imread(pics[1])
+    _img = img
+    if len(pics) > 2:
+        for tt in range(2, len(pics)):
+            os.remove(pics[tt])
 
-  for (x, y, w, h) in faces:
-    _x = x
-    _y = y
-    _w = w
-    _h = h
+    # detect face and eyes
+    if activate_logic == True:
+        if check == 1:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+            face_detected = 0
 
-    face_detected = 1
-    face_size = math.sqrt(w * w + y * y)
-    roi_gray = gray[y:y + h, x:x + w]
-    roi_color = img[y:y + h, x:x + w]
+            # identify face
 
-    open_eyes_detected = 0
-    eyes_tilt = eye_cascade.detectMultiScale(gray[y:y + h, x:x + w], 1.1, 4)
-    eyes = eye_cascade.detectMultiScale(roi_gray, 1.25, 5)
-    index = 0
-    eye_1 = [None, None, None, None]
-    eye_2 = [None, None, None, None]
+            for (x, y, w, h) in faces:
+                _x = x
+                _y = y
+                _w = w
+                _h = h
 
-    for (ex, ey, ew, eh) in eyes:
-      open_eyes_detected = 1
-      cv2.putText(img, 'Eyes Open: ' + str(len(eyes)), (10, height - 30), 0, 0.6, (0, 255, 0))
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                face_detected = 1
+                face_size = math.sqrt(w * w + y * y)
+                # cv2.putText(img,"Face Size : " + str(int(face_size)), (10, height - 50), 0, 0.6, (0, 255, 0))
+                roi_gray = gray[y : y + h, x : x + w]
+                roi_color = img[y : y + h, x : x + w]
 
-    for (ex, ey, ew, eh) in eyes_tilt:
-      if index == 0:
-        eye_1 = [ex, ey, ew, eh]
-      elif index == 1:
-        eye_2 = [ex, ey, ew, eh]
+                # ### eyes = eye_cascade.detectMultiScale(roi_gray, 1.25,5)
+                # identify open eyes
+                open_eyes_detected = 0
+                eyes_tilt = eye_cascade.detectMultiScale(
+                    gray[y : y + h, x : x + w], 1.1, 4
+                )
+                eyes = eye_cascade.detectMultiScale(roi_gray, 1.25, 5)
+                index = 0
+                eye_1 = [None, None, None, None]
+                eye_2 = [None, None, None, None]
 
-      index = index + 1
-      if eye_1[0] is not None and eye_2[0] is not None:
-        if eye_1[0] < eye_2[0]:
-          left_eye = eye_1
-          right_eye = eye_2
-        else:
-          left_eye = eye_2
-          right_eye = eye_1
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(
+                        roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2
+                    )
+                    open_eyes_detected = 1
+                    cv2.putText(
+                        img,
+                        "Eyes Open: " + str(len(eyes)),
+                        (10, height - 30),
+                        0,
+                        0.6,
+                        (0, 255, 0),
+                    )
 
-        left_eye_center = (int(left_eye[0] + left_eye[2] / 2), int(left_eye[1] + left_eye[3] / 2))
-        right_eye_center = (int(right_eye[0] + right_eye[2] / 2), int(right_eye[1] + right_eye[3] / 2))
+                for (ex, ey, ew, eh) in eyes_tilt:
+                    if index == 0:
+                        eye_1 = [ex, ey, ew, eh]
+                    elif index == 1:
+                        eye_2 = [ex, ey, ew, eh]
+                    # cv2.rectangle(img[y:y + h, x:x + w], (ex, ey), (ex
+                    #             + ew, ey + eh), (0, 0, 255), 2)
+                    index = index + 1
+                    if eye_1[0] is not None and eye_2[0] is not None:
+                        if eye_1[0] < eye_2[0]:
+                            left_eye = eye_1
+                            right_eye = eye_2
+                        else:
+                            left_eye = eye_2
+                            right_eye = eye_1
+                        left_eye_center = (
+                            int(left_eye[0] + left_eye[2] / 2),
+                            int(left_eye[1] + left_eye[3] / 2),
+                        )
 
-        left_eye_x = left_eye_center[0]
-        left_eye_y = left_eye_center[1]
-        right_eye_x = right_eye_center[0]
-        right_eye_y = right_eye_center[1]
+                        right_eye_center = (
+                            int(right_eye[0] + right_eye[2] / 2),
+                            int(right_eye[1] + right_eye[3] / 2),
+                        )
 
-        delta_x = right_eye_x - left_eye_x
-        delta_y = right_eye_y - left_eye_y
-        if delta_x == 0:
-          continue
+                        left_eye_x = left_eye_center[0]
+                        left_eye_y = left_eye_center[1]
+                        right_eye_x = right_eye_center[0]
+                        right_eye_y = right_eye_center[1]
 
-        angle = np.arctan(delta_y / delta_x)
-        # Converting radians to degrees
-        angle = angle * 180 / np.pi
+                        delta_x = right_eye_x - left_eye_x
+                        delta_y = right_eye_y - left_eye_y
+                        if delta_x == 0:
+                            continue
 
-        # Provided a margin of error of 10 degrees
-        # (i.e, if the face tilts more than 10 degrees
-        # on either side the program will classify as right or left tilt)
+                        # Slope of line formula
+                        angle = np.arctan(delta_y / delta_x)
 
-        if angle > 10:
-          tilted = True
-          cv2.putText(img, 'RIGHT TILT :' + str(int(angle)) + ' degrees', (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_4)
-        elif angle < -10:
-          tilted = True
-          cv2.putText(img, 'LEFT TILT :' + str(int(angle)) + ' degrees', (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_4)
+                        # Converting radians to degrees
+                        angle = angle * 180 / np.pi
 
-      smile, rejectLevels, levelWeights = smile_cascade.detectMultiScale3(roi_gray, 1.25, 5, outputRejectLevels=True)
-      if len(levelWeights) == 0:
-        smile_detected = 0
-        cv2.putText(img, 'Not Smiling', (400, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
-      else:
-          if max(levelWeights) < 2:
-            smile_detected = 0
-            cv2.putText(img, 'Not Smiling', (400, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
-          else:
-            smile_detected = 1
-            cv2.putText(img, 'Smiling', (400, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
+                        # Provided a margin of error of 10 degrees
+                        # (i.e, if the face tilts more than 10 degrees
+                        # on either side the program will classify as right or left tilt)
 
-  cv2.imshow('Frame', img)
-  cv2.waitKey(10)
+                        if angle > 10:
+                            tilted = True
+                            cv2.putText(
+                                img,
+                                "RIGHT TILT :" + str(int(angle)) + " degrees",
+                                (20, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                1,
+                                (255, 255, 255),
+                                2,
+                                cv2.LINE_4,
+                            )
+                        elif angle < -10:
+                            tilted = True
+                            cv2.putText(
+                                img,
+                                "LEFT TILT :" + str(int(angle)) + " degrees",
+                                (20, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                1,
+                                (255, 255, 255),
+                                2,
+                                cv2.LINE_4,
+                            )
+                        else:
+                            tilted = False
+
+                smile, rejectLevels, levelWeights = smile_cascade.detectMultiScale3(
+                    roi_gray, 1.25, 5, outputRejectLevels=True
+                )
+                if len(levelWeights) == 0:
+                    smile_detected = 0
+                    cv2.putText(
+                        img,
+                        "Not Smiling",
+                        (400, 20),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 255, 0),
+                        1,
+                    )
+                else:
+                    if max(levelWeights) < 2:
+                        smile_detected = 0
+                        cv2.putText(
+                            img,
+                            "Not Smiling",
+                            (400, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5,
+                            (255, 255, 0),
+                            1,
+                        )
+                    else:
+                        smile_detected = 1
+                        cv2.putText(
+                            img,
+                            "Smiling",
+                            (400, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.5,
+                            (255, 255, 0),
+                            1,
+                        )
+
+    #                 if len(eyes) == 2 and smile_detected == 0 and tilted is False:
+    #                     print(len(eyes))
+    #                     print(smile_detected)
+    # print(tilted)
+    # print("take photo")
+    # cv2.putText(img,"Eyes Open: " + str(len(eyes)), (10, height - 30), 0, 0.6, (0, 255, 0))
+    # os.killpg(p.pid, signal.SIGTERM)
+    # cv2.destroyAllWindows()
+    # cv2.imwrite('Frame2444.jpg',img)
+    # with Image.open('Frame2444.jpg') as img:
+    # img.load()
+    # cropped_img = img.crop((x-40, y-40, x + w + 40, y + h + 40))
+    # cropped_img.show()
+    # subprocess.run(['libcamera-still --width 640 --height 480 -o 64mp-autofocus-test-new.jpg --autofocus'], shell=True)
+    #                         sys.exit(1)
+
+    cv2.imshow("Frame", img)
+    cv2.waitKey(10)
